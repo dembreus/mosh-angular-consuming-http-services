8950700c37674654b6d5fb0ded6458cd37f360d6
Dillon Embreus <dillon.embreus@treehousei.net>
2022-05-27 20:41:13 -0600

    Starting routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { NotFoundComponent } from "./components/not-found/not-found.component";
+import { PostComponent } from "./components/posts/post/post.component";
+import { PostsComponent } from "./components/posts/posts.component";
+import { UsersComponent } from "./components/users/users.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "core/lib/images/app", component: AppComponent },
+  { path: "posts/:id", component: PostComponent },
+  { path: "posts", component: PostsComponent },
+  { path: "users", component: UsersComponent },
+  { path: "", component: AppComponent },
+  { path: "**", component: NotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
