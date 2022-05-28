45a237f93c993581790659a1b0d826122de8ca48
Dillon Embreus <dillon.embreus@treehousei.net>
2022-05-28 01:36:37 -0600

    updated routing. redifined nav, main and footer content.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AppComponent } from "./app.component";
+import { HomeComponent } from "./components/home/home.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { PostComponent } from "./components/posts/post/post.component";
 import { PostsComponent } from "./components/posts/posts.component";
 import { UsersComponent } from "./components/users/users.component";
 
 const routes: Routes = [
-  { path: "core/lib/images/app", component: AppComponent },
   { path: "posts/:id", component: PostComponent },
   { path: "posts", component: PostsComponent },
   { path: "users", component: UsersComponent },
-  { path: "", component: AppComponent },
+  { path: "", component: HomeComponent },
   { path: "**", component: NotFoundComponent },
 ];
 
