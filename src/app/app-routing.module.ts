d729478cef0b22ecb07fba5125f78873a0c8c2bc
Dillon <dillon.embreus@gmail.com>
2022-11-04 21:08:59 -0600

    Added Git hub followers and profile components to the routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { GithubFollowersComponent } from "./components/github-followers/github-followers.component";
+import { GithubProfileComponent } from "./components/github-profile/github-profile.component";
 import { HomeComponent } from "./components/home/home.component";
 import { NotFoundComponent } from "./components/not-found/not-found.component";
 import { PostComponent } from "./components/posts/post/post.component";
@@ -7,6 +9,9 @@ import { PostsComponent } from "./components/posts/posts.component";
 import { UsersComponent } from "./components/users/users.component";
 
 const routes: Routes = [
+  { path: "followers", component: GithubFollowersComponent },
+  { path: "profile/:username", component: GithubProfileComponent },
+  { path: "posts/:id", component: PostComponent },
   { path: "posts/:id", component: PostComponent },
   { path: "posts", component: PostsComponent },
   { path: "users", component: UsersComponent },
